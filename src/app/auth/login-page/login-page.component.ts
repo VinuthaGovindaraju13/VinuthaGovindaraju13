06e31014c67bec56d0ac0109c40de4b47f56531e
Vinutha Govindaraju <vinutha.govindaraju@tech9.com>
2023-05-23 23:42:02 +0530

    initial commit

---
+++ src/app/auth/login-page/login-page.component.ts
@@ -0,0 +1,11 @@
+import {Component, Inject,  OnInit} from "@angular/core";
+
+@Component({
+    selector: 'login-page',
+    templateUrl: './login-page.component.html'
+})
+
+export class LoginPageComponent{
+
+
+}
